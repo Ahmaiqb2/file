82ac25aa2b111a22278c6e6f7d7377670ddd6496
Ahmaiqb <Ahsa0084@stud.kea.dk>
2021-10-21 14:27:07 +0200

    FileOgpaver

--- src/FileOpgaverKea/FileHandler.java
+++ src/FileOpgaverKea/FileHandler.java
@@ -9,12 +9,52 @@ import java.util.Scanner;
 
 public class FileHandler {
 
-    public void filLæser(String filNavn, String data) throws FileNotFoundException {
+    //Opgave 1, Uppercase på bogstaver over 5
+    public void filLæser(String filNavn) throws FileNotFoundException {
+
         File file = new File(filNavn);
         Scanner scanner = new Scanner(file);
+
+        int ordLength = 5;
+
         while (scanner.hasNext()){
-            System.out.println(scanner.nextLine());
+            String data = scanner.nextLine();
+            String[] ordliste = data.split(" ");
+            String ordet = "";
+            for (int i = 0; i < ordliste.length; i++){
+                String ord = ordliste[i];
+                if (ord.length() >= ordLength){
+                    ord = ord.toUpperCase();
+                }
+                ordet += ord + " ";
+            }
+            System.out.println(ordet);
         }
+    }
+
+    //Opgave 2, finde gennemsnitstemperaturen af byerne.
+    public void filLæser2(String filNavn) throws FileNotFoundException {
+        File file = new File(filNavn);
+        Scanner scanner = new Scanner(file);
+
+        double sum = 0;
+        double temp = 0;
+        int count = 0;
+        String by;
+
+        System.out.println("Byerne har af temperatur: ");
+        while (scanner.hasNext()) {
+            by = scanner.next();
+
+            if (scanner.hasNextInt()) {
+                temp = scanner.nextInt();
+                sum += temp;
+            }
+            count++;
+            System.out.printf(" - %s: %.0f grader\n", by, temp);
+        }
+        double gnsTemp = sum / count;
+        System.out.println("\nGennemsnitstemperaturen: " + Math.round(gnsTemp) + " grader");
     }
 
     public void filSkriver(String filNavn, String data) throws FileNotFoundException {
@@ -22,9 +62,5 @@ public class FileHandler {
             PrintStream ps = new PrintStream(new FileOutputStream(file, true));
             ps.println(data);
             ps.close();
-    }
-
-    public void deleteContents(String filNavn) throws FileNotFoundException{
-        new PrintStream(filNavn).close();
     }
 }
--- src/FileOpgaverKea/læsFraFile.java
+++ src/FileOpgaverKea/læsFraFile.java
@@ -6,19 +6,29 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class læsFraFile {
-    public static void main(String[] args) {
-        File file = new File("opgaver/læsFra.txt");
-        String linje = "";
-        try {
-            Scanner scanner = new Scanner(file);
-            linje = scanner.nextLine();
-            String[] ordListe = linje.split(" ");
-            ordListe[1].toUpperCase();
-            System.out.println(linje);
-            linje = scanner.nextLine();
-            System.out.println(linje);
+    public static void main(String[] args) throws FileNotFoundException {
 
+        FileHandler fh = new FileHandler();
+
+        System.out.println("""
+        Opgave 1!
+        """);
+        try {
+            fh.filLæser("opgaver/læsFra.txt");
         } catch (FileNotFoundException e) {
+            System.out.println("Der er en fejl");
+            e.printStackTrace();
+        }
+
+        System.out.println("\n------------------------------\n");
+
+        System.out.println("""
+        Opgave 2!
+        """);
+        try {
+            fh.filLæser2("opgaver/DMI");
+        } catch (FileNotFoundException e) {
+            System.out.println("Der er en fejl");
             e.printStackTrace();
         }
     }
